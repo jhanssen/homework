4ab2d63b6a0635a3b8036894db62e4c42bb64000
Jan Erik Hanssen <jhanssen@netflix.com>
2015-05-21 13:15:36 -0700

    completion work

--- src/console/console.js
+++ src/console/console.js
@@ -8,6 +8,7 @@ var readline = require("readline");
 var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
+    completer: completer,
     terminal: true
 });
 
@@ -15,6 +16,50 @@ function prompt()
 {
     rl.setPrompt("> ", 2);
     rl.prompt();
+}
+
+var pendingCompletions = {id: 0, pending: Object.create(null)};
+
+function completer(line, callback)
+{
+    var completions = {
+        candidates: ["help ", "controllers ", "quit ", "set "],
+        set: {
+            candidates: ["controller ", "sensor ", "rule ", "scene "],
+            controller: {
+                candidates: function() {
+                    var id = pendingCompletions.id;
+                    pendingCompletions.pending[id] = callback;
+                    send({get: "controllers", id: id});
+                    ++pendingCompletions.id;
+                }
+            }
+        }
+    };
+
+    var hits;
+    var candidate = completions;
+    var splitLine = line.split(' ');
+    // console.log(JSON.stringify(splitLine));
+    var used = line;
+    for (var i = 0; i < splitLine.length; ++i) {
+        if (candidate.hasOwnProperty(splitLine[i])) {
+            candidate = candidate[splitLine[i]];
+            var sofar = splitLine[i] + " ";
+            // eat spaces
+            while (i + 1 < splitLine.length && splitLine[i + 1].length == 0) {
+                sofar += " ";
+                ++i;
+            }
+            used = used.substr(sofar.length);
+        } else {
+            break;
+        }
+    }
+    hits = candidate.candidates.filter(function(c) { return c.indexOf(splitLine[i]) == 0; });
+    // show all completions if none found
+    // console.log("callback " + JSON.stringify([hits.length ? hits : candidate.candidates, used]));
+    callback(null, [hits.length ? hits : candidate.candidates, used]);
 }
 
 function showHelp()
