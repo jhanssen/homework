a8a8b1751450689002cb4b2251b1d942427baf69
Jan Erik Hanssen <jhanssen@gmail.com>
2016-02-18 22:13:52 -0800

    Fancy UI

--- html/index.js
+++ html/index.js
@@ -1,8 +1,8 @@
-/*global angular,WebSocket,EventEmitter*/
+/*global angular,WebSocket,EventEmitter,clearTimeout,setTimeout*/
 
 "use strict";
 
-var module = angular.module('app', ['ui.bootstrap']);
+var module = angular.module('app', ['ui.bootstrap', 'ui.bootstrap-slider', 'frapontillo.bootstrap-switch']);
 module.controller('mainController', function($scope) {
     // // BUTTONS ======================
 
@@ -15,6 +15,8 @@ module.controller('mainController', function($scope) {
 
     // // COLLAPSE =====================
     // $scope.isCollapsed = false;
+
+    $scope.Type = { Dimmer: 0, Light: 1, Fan: 2 };
 
     $scope.listener = new EventEmitter();
     $scope.id = 0;
@@ -45,19 +47,99 @@ module.controller('mainController', function($scope) {
         // request devices
         $scope.request({ type: "devices" }).then((response) => {
             console.log("got devices", response);
-            $scope.devices = response;
-            $scope.$apply();
+            if (!(response instanceof Array))
+                return;
+            var devs = Object.create(null);
+            var rem = response.length;
+            var done = () => {
+                console.log("got all device values");
+
+                // fixup devices
+                for (var k in devs) {
+                    let dev = devs[k];
+                    switch (dev.type) {
+                    case $scope.Type.Dimmer:
+                        dev._timeout = undefined;
+                        Object.defineProperty(dev, "level", {
+                            get: function() {
+                                return dev.values.level.raw;
+                            },
+                            set: function(v) {
+                                if (typeof v === "number") {
+                                    if (dev._timeout != undefined)
+                                        clearTimeout(dev._timeout);
+                                    dev._timeout = setTimeout(() => {
+                                        $scope.request({ type: "setValue", devuuid: dev.uuid, valname: "level", value: v });
+                                        delete dev._timeout;
+                                    }, 500);
+                                } else {
+                                    throw "invalid value type for dimmer: " + (typeof v);
+                                }
+                            }
+                        });
+                        break;
+                    case $scope.Type.Light:
+                        Object.defineProperty(dev, "value", {
+                            get: function() {
+                                return dev.values.value.raw != 0;
+                            },
+                            set: function(a) {
+                                $scope.request({ type: "setValue", devuuid: dev.uuid, valname: "value", value: a ? 1 : 0 });
+                            }
+                        });
+                        break;
+                    }
+                }
+
+                $scope.devices = devs;
+                $scope.$apply();
+            };
+            for (var i = 0; i < response.length; ++i) {
+                let uuid = response[i].uuid;
+                devs[uuid] = response[i];
+                devs[uuid].values = Object.create(null);
+                $scope.request({ type: "values", devuuid: uuid }).then((response) => {
+                    if (response instanceof Array) {
+                        for (var i = 0; i < response.length; ++i) {
+                            let val = response[i];
+                            devs[uuid].values[val.name] = val;
+                        }
+                    }
+                    if (!--rem)
+                        done();
+                });
+            }
         });
     };
-    $scope.updateValue = () => {
+    $scope.updateValue = (updated) => {
+        if (typeof $scope.devices === "object") {
+            if (updated.devuuid in $scope.devices) {
+                const dev = $scope.devices[updated.devuuid];
+                if (updated.valname in dev.values) {
+                    const val = dev.values[updated.valname];
+                    const old = {
+                        value: val.value,
+                        raw: val.raw
+                    };
+                    val.value = updated.value;
+                    val.raw = updated.raw;
+                    $scope.listener.emitEvent("valueUpdated", [val, old, dev]);
+                } else {
+                    console.log("value updated but value not known", updated.devuuid, updated.valname);
+                }
+            } else {
+                console.log("value for unknown device, discarding", updated.devuuid, updated.valname);
+            }
+        }
     };
     $scope.handleResponse = () => {
     };
 
     $scope.socket = new WebSocket(`ws://${window.location.hostname}:8093/`);
     $scope.socket.onmessage = (evt) => {
+        var msg;
         try {
-            var msg = JSON.parse(evt.data);
+            msg = JSON.parse(evt.data);
         } catch (e) {
             console.error("can't JSON parse", evt.data);
             return;
